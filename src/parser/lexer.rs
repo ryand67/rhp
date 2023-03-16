d55b0c01a872196282d83bfadf63de080b260911
Ryan Devenney <rwdevenney@gmail.com>
2023-03-15 21:27:17 -0400

    lex loop

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,4 +1,5 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use crate::util::open_file;
+use std::{io::BufRead, path::PathBuf};
 
 pub struct Lexer {
     files: Vec<PathBuf>,
@@ -6,16 +7,31 @@ pub struct Lexer {
 
 impl Lexer {
     pub fn new(files: Vec<PathBuf>) -> Self {
-        Self { files }
+        return Self { files };
     }
 
     pub fn lex_all_files(&mut self) {
         for file in &self.files {
-            self.lex_file(&file);
+            match self.lex_file(&file) {
+                Ok(_) => (),
+                Err(e) => println!("Error opening file: {:?}", e),
+            }
         }
     }
 
-    fn lex_file(&self, file: &PathBuf) {
-        todo!();
+    fn lex_file(&self, path: &PathBuf) -> Result<(), String> {
+        let mut reader = match open_file(path) {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(format!("{}", e.to_string()));
+            }
+        };
+
+        let mut line = String::new();
+        while reader.read_line(&mut line).unwrap_or(0) != 0 {
+            todo!();
+        }
+
+        return Ok(());
     }
 }
