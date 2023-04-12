dc0b39b34177ff569a10fa452f844c45c8991f0f
Ryan Devenney <rwdevenney@gmail.com>
2023-04-11 21:09:30 -0400

    lexer refactor

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,18 +1,25 @@
-use crate::parser::token::*;
 use crate::util::open_file;
-use std::{io::BufRead, path::PathBuf};
+use std::{io::Read, path::PathBuf};
+
+type CharPos = (char, usize);
 
 pub struct Lexer {
     files: Vec<PathBuf>,
+    curr: CharPos,
+    content: String,
 }
 
 impl Lexer {
     pub fn new(files: Vec<PathBuf>) -> Self {
-        return Self { files };
+        return Self {
+            files,
+            curr: ('\0', 0),
+            content: String::default(),
+        };
     }
 
     pub fn lex_all_files(&mut self) {
-        for file in &self.files {
+        for file in self.files.clone() {
             match self.lex_file(&file) {
                 Ok(_) => (),
                 Err(e) => println!("Error opening file: {:?}", e),
@@ -20,7 +27,11 @@ impl Lexer {
         }
     }
 
-    fn lex_file(&self, path: &PathBuf) -> Result<(), String> {
+    fn set_content(&mut self, content: String) {
+        self.content = content;
+    }
+
+    fn lex_file(&mut self, path: &PathBuf) -> Result<(), String> {
         let mut reader = match open_file(path) {
             Ok(r) => r,
             Err(e) => {
@@ -28,28 +39,22 @@ impl Lexer {
             }
         };
 
-        let mut line_buf = String::new();
-        let mut mid_parse = true;
-        let mut tag_buf = String::new();
+        let mut buf = String::new();
 
-        while reader.read_line(&mut line_buf).unwrap_or(0) != 0 {
-            if line_buf.ends_with("\n") {
-                line_buf = line_buf.replace("\n", "");
-            }
+        reader.read_to_string(&mut buf).unwrap();
+        self.set_content(buf);
 
-            if mid_parse
-                && line_buf.trim().starts_with("<")
-                && line_buf.ends_with(">")
-                && !line_buf.ends_with("/>")
-            {
-                tag_buf.push_str(&line_buf);
-            }
-        }
+        self.lex_content();
 
         return Ok(());
     }
 
-    fn gather_content(&self, content: String) -> String {
-        todo!();
+    fn lex_content(&mut self) {
+        let chars: Vec<char> = self.content.chars().collect();
+        self.curr = (chars[0], 0);
+
+        chars.into_iter().enumerate().for_each(|(i, c)| {
+            todo!();
+        });
     }
 }
--- src/parser/token.rs
+++ src/parser/token.rs
@@ -6,8 +6,6 @@ pub struct Tag {
     tag_type: TagType,
 }
 
-/// Test
 pub fn tokenize(tag_buf: &String) -> Tag {
-    println!("OH");
     todo!();
 }
